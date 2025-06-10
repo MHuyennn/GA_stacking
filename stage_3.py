0d468c13a14da391e2496421e5f214aa8e74c1c9
MHuyennn <sakiko.akare@gmail.com>
2025-06-11 00:31:24 +0700

    Update stage_3.py

--- stage_3.py
+++ stage_3.py
@@ -67,7 +67,6 @@ def run_stage_3(data_dir, data_path, pretrained_dir, cache_dir, meta_dir):
     ga.run()
 
 if __name__ == "__main__":
-    # Ví dụ mặc định, có thể thay đổi trong Notebook
     run_stage_3(
         data_dir='/kaggle/input/covid19-radiography-database/COVID-19_Radiography_Dataset',
         data_path='/kaggle/working/data/covid19_radiography_data.npz',
